afe44875aa90f3d09d1b73de833cc15f087d0f87
Vít Škvára <vit.skvara@gmail.com>
2019-05-16 14:20:47 +0200

    fixed dependencies

--- src/FewShotAnomalyDetection.jl
+++ src/FewShotAnomalyDetection.jl
@@ -21,12 +21,6 @@ include("svae_vamp.jl")
 include("svae_vamp_means.jl")
 include("vae.jl")
 
-if in("CuArrays",keys(Pkg.installed())) && in("CUDAnative",keys(Pkg.installed()))
-    using CuArrays
-    using CUDAnative
-    include("svae_gpu.jl")
-end
-
 export SVAEbase, SVAEtwocaps, SVAEvamp, SVAEvampmeans, VAE, loss, wloss, log_pxexpectedz, pz, log_pz, log_px, log_pz_jacobian_encoder, log_pz_jacobian_decoder, samplez, zparams, printing_wloss, mem_wloss, log_det_jacobian_encoder, log_det_jacobian_decoder, hsplit1softp
 
 end
