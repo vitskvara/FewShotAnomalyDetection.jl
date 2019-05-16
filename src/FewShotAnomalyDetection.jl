453337f52d626ffe51d2f5cfff82bcb21c9bcd96
Vít Škvára <vit.skvara@gmail.com>
2019-05-16 14:55:12 +0200

    removed the vae dependency for now

--- src/FewShotAnomalyDetection.jl
+++ src/FewShotAnomalyDetection.jl
@@ -19,7 +19,7 @@ include("svaebase.jl")
 include("svaetwocaps.jl")
 include("svae_vamp.jl")
 include("svae_vamp_means.jl")
-include("vae.jl")
+#include("vae.jl")
 
 export SVAEbase, SVAEtwocaps, SVAEvamp, SVAEvampmeans, VAE, loss, wloss, log_pxexpectedz, pz, log_pz, log_px, log_pz_jacobian_encoder, log_pz_jacobian_decoder, samplez, zparams, printing_wloss, mem_wloss, log_det_jacobian_encoder, log_det_jacobian_decoder, hsplit1softp
 
